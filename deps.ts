ec65897385b03d4e1e323bac4882356cb2c6a857
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-05-01 23:16:46 +0900

    dependency: resolve type name

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export * from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 export * as dotenv from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 export * from "npm:@microsoft/microsoft-graph-client";
-export * from "npm:@microsoft/microsoft-graph-types";
-export * from "npm:@googleapis/classroom";
+export * as GraphApi from "npm:@microsoft/microsoft-graph-types";
+export * as ClassroomApi from "npm:@googleapis/classroom";
