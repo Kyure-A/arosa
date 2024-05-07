f5142b09f7d343b9714781065ad0194f8798e906
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-05-08 01:39:59 +0900

    dependency: rename export

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export * from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 export * as dotenv from "https://deno.land/std@0.224.0/dotenv/mod.ts";
-export * from "npm:@microsoft/microsoft-graph-client";
-export * as GraphApi from "npm:@microsoft/microsoft-graph-types";
-export * as ClassroomApi from "npm:@googleapis/classroom";
+export * as mg from "npm:@microsoft/microsoft-graph-client";
+export * as mgt from "npm:@microsoft/microsoft-graph-types";
+export * as ca from "npm:@googleapis/classroom";
